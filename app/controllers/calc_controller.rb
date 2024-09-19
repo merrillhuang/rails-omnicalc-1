d07c445e060d3d9e1651263423d637ba2282d9be
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-19 20:42:47 +0000

    create layout for square_results

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -5,7 +5,7 @@ class CalcController < ApplicationController
 
   def square_results
     @input = params.fetch("number")
-    @result = (@input ** 2).to_d
+    @result = (@input.to_f) ** 2
 
     render({ template: "calc_templates/square_result"})
   end
