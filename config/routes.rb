7e4912cae0b95635e92339d7e378f574349d1370
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-19 20:49:35 +0000

    add routes for new square root and results

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get("/square/new", { controller: "calc", action: "new_square"})
 
   get("/square/results", { controller: "calc", action: "square_results"})
+
+  get("/square_root/new", { controller: "calc", action: "new_square_root"})
+
+  get("/square_root/results", { controller: "calc", action: "square_root_results"})
 end
